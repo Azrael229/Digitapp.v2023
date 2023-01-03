6b34730973cc9cd8bc102f0ae77d618c998edaee
azrael229 <israel.navarrete229@gmail.com>
2023-01-03 13:03:24 -0600

    tarjetas de productos con estilo

---
+++ Productos/require_productos.php
@@ -0,0 +1,29 @@
+<?php
+
+    require ("conexion.php");
+
+    $resp = $conexion->query(
+
+        "SELECT  *
+        FROM productos"
+        
+    ) or die(print($conexion->errorInfo()));
+    $data = [];
+
+    while($item = $resp->fetch(PDO::FETCH_OBJ)){
+        $data[] = [
+       //--variable--/--base de datos--
+            "ID" => $item->id_producto,
+            "imagen" => $item->imagen,
+            "subcategoria" => $item->subcategoria,
+            "marca" => $item->marca,
+            "modelo" => $item->modelo,
+            "capacidad" => $item->capacidad,
+            "resolucion" => $item->resolucion,
+            "descripcion" => $item->descripcion,
+            "precio_publico" => $item->precio_publico,          
+        ];
+    }
+            // echo $data;
+    echo json_encode($data);
+?>
--- js/todos_los_productos.js
+++ js/todos_los_productos.js
@@ -1,56 +1,77 @@
-var aRelojes = [
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Hombre',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Mujer',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Hombre',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Mujer',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Hombre',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-    {
-        imagen: './img/buzo-adidas.jpg',
-        nombre: 'Rolex',
-        categoria:'Mujer',
-        precio: 4999,
-        descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
-    },
-];
+// var aRelojes = [
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Hombre',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Mujer',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Hombre',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Mujer',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Hombre',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+//     {
+//         imagen: './img/buzo-adidas.jpg',
+//         nombre: 'Rolex',
+//         categoria:'Mujer',
+//         precio: 4999,
+//         descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+//     },
+// ];
 
-let tarjetas = document.getElementById("area_tarjetas_productos");
+var tarjetas = document.getElementById("area_tarjetas_productos");
 
-aRelojes.map(item =>{
+// aRelojes.map(item =>{
 
-    tarjetas.innerHTML +=`<div class="cards">
-    <p>${item.nombre}</p>
-    <p>${item.categoria}</p>
-    <p>${item.descripcion}</p>
-    <p>${item.precio}</p>
-    </div>`;
-});
+//     tarjetas.innerHTML +=`<div class="cards">
+//     <p>${item.nombre}</p>
+//     <p>${item.categoria}</p>
+//     <p>${item.descripcion}</p>
+//     <p>${item.precio}</p>
+//     </div>`
+// });
+
+function crearTarjetas(){
+    var urlReqProd = "Productos/require_productos.php"
+
+    fetch(urlReqProd)
+    .then(res => res.json())
+    .then(data => { 
+        data.map(item =>{
+            tarjetas.innerHTML +=`<div class="cards">
+            <img src="img/${item.imagen}" width="200" height="200">
+            <p>${item.subcategoria}</p>
+            <p>Marca: ${item.marca}</p>
+            <p>Modelo:${item.modelo}</p>
+            <p>Capacidad: ${item.capacidad}</p>
+            <p>Resolución: ${item.resolucion}</p>
+            <p>$ ${item.precio_publico}</p>
+            </div>`
+         });
+    });
+}
+crearTarjetas();
