f19fda264656568364e52729950867ef7113c39d
azrael229 <israel.navarrete229@gmail.com>
2023-02-23 22:54:37 -0600

    console log datos para ingresar productos

--- js/tarjetas_productos.js
+++ js/tarjetas_productos.js
@@ -102,21 +102,21 @@ formulario.addEventListener("submit", function(e){
 
     var url_insert = "Productos/insert_update.php";  
     const datos = new FormData(formulario);
-    // console.log(datos)
-    // console.log(datos.get("categoria"))
-    // console.log(datos.get("subcategoria"))
-    // console.log(datos.get("Producto"))
-    // console.log(datos.get("Marca"))
-    // console.log(datos.get("Modelo"))
-    // console.log(datos.get("Capacidad"))
-    // console.log(datos.get("Resolucion"))
-    // console.log(datos.get("descripProducto"))
-    // console.log(datos.get("imagen"))
-    // console.log(datos.get("proveedor"))
-    // console.log(datos.get("precioDist"))
-    // console.log(datos.get("actual_precio"))
-    // console.log(datos.get("factor"))
-    // console.log(datos.get("precioPublico"))
+    console.log(datos)
+    console.log(datos.get("categoria"))
+    console.log(datos.get("subcategoria"))
+    console.log(datos.get("Producto"))
+    console.log(datos.get("Marca"))
+    console.log(datos.get("Modelo"))
+    console.log(datos.get("Capacidad"))
+    console.log(datos.get("Resolucion"))
+    console.log(datos.get("descripProducto"))
+    console.log(datos.get("imagen"))
+    console.log(datos.get("proveedor"))
+    console.log(datos.get("precioDist"))
+    console.log(datos.get("actual_precio"))
+    console.log(datos.get("factor"))
+    console.log(datos.get("precioPublico"))
 
     fetch(url_insert,{
         method: "POST",
