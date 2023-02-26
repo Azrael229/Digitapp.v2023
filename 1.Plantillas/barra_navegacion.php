3effe38d3599aeab5290e7b295716a02a908067f
azrael229 <israel.navarrete229@gmail.com>
2023-02-25 20:26:27 -0600

    cambios barra nav

--- 1.Plantillas/barra_navegacion.php
+++ 1.Plantillas/barra_navegacion.php
@@ -1,25 +1,30 @@
-<nav>
-    <img class="logo" src="img/LogoMakr_4Pdzl8.png" alt="logo" />
-    
-    <ul class="enlaces-menu">
+
+
+
+<header class="barra_navegacion" >
+
+
+    <nav class="barra_navegacion">
+        <img class="logo" src="img/LogoMakr_4Pdzl8.png" alt="logo" />
         
-        <li><a href="clientes.php" id="btn_">CLIENTES</a></li>
-        <li><a href="index.php">PRODUCTOS</a></li>
+        <ul class="enlaces-menu">
+            
+            <li><a href="clientes.php" id="btn_">CLIENTES</a></li>
+            <li><a href="index.php">PRODUCTOS</a></li>
+            
+
+        </ul>
         
 
-    </ul>
-    
+    </nav>
 
-</nav>
+    <div class="barra_navegacion" id="header">
+        <i class="fa-solid fa-arrow-rotate-left" id="ico_flecha_regresar" ></i>
 
+        <h1 id="titulo_header"><i class="fa-solid fa-boxes-stacked"></i> Productos</h1>
 
-<header class="header">
-    
-    <i class="fa-solid fa-arrow-rotate-left" id="ico_flecha_regresar" ></i>
-
-    <h1 id="titulo_header"><i class="fa-solid fa-boxes-stacked"></i> Productos</h1>
-
-    
-    <div id="btn_cotizacion">  <i class="fa-regular fa-file-lines" id="ico_cotizacion">  <span id="span_contador">0</span></i> </div>
+        
+        <div id="btn_cotizacion">  <i class="fa-regular fa-file-lines" id="ico_cotizacion">  <span id="span_contador">0</span></i> </div>
+    </div>
 
 </header>
